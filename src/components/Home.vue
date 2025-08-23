c661d1b7121d0624073aad7350ca30b49f8df32d
Septian Adi Pratama <septianadip7998@gmail.com>
2025-08-24 00:32:35 +0700

    feat: add restaurant data to JSON and enhance Home component to display restaurant details

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,52 @@
 <template>
   <Header />
   <h1>Hello {{ name }}, Welcome to Home</h1>
+  <table border="1">
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Contact</th>
+      <th>Address</th>
+    </tr>
+    <tr v-for="item in restaurants" :key="item.id">
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
 export default {
   name: 'Home',
   data() {
     return {
-      name: ''
+      name: '',
+      restaurants: []
     };
   },
   components: {
     Header
   },
-  mounted() {
+  async mounted() {
     let user = localStorage.getItem('user-info');
     this.name = JSON.parse(user).name;
     if (!user) {
       this.$router.push({ name: 'SignUp' });
     }
+    let result = await axios.get('http://localhost:3000/restaurants');
+    console.warn(result);
+    this.restaurants = result.data;
   }
 };
 </script>
+
+<style>
+td {
+  width: 160px;
+  height: 40px;
+}
+</style>
