a1265e561983f06c03b86408d4d9c5a7dbf0cbdb
Septian Adi Pratama <septianadip7998@gmail.com>
2025-08-23 23:33:30 +0700

    feat: update Header component to use router-links and enhance Home component to display user name

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <Header />
-  <h1>Hello User, Welcome to Home</h1>
+  <h1>Hello {{ name }}, Welcome to Home</h1>
 </template>
 
 <script>
 import Header from './Header.vue';
 export default {
   name: 'Home',
+  data() {
+    return {
+      name: ''
+    };
+  },
   components: {
     Header
   },
   mounted() {
     let user = localStorage.getItem('user-info');
+    this.name = JSON.parse(user).name;
     if (!user) {
       this.$router.push({ name: 'SignUp' });
     }
