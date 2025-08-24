1823bd12566cf3b0117612aca47aaac226d59903
Septian Adi Pratama <septianadip7998@gmail.com>
2025-08-24 23:28:35 +0700

    feat: update Home component to include delete functionality and enhance restaurant display

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,8 +14,9 @@
       <td>{{ item.name }}</td>
       <td>{{ item.contact }}</td>
       <td>{{ item.address }}</td>
-      <td>
+      <td style="display: flex; gap: 10px; justify-content: center; align-items: center">
         <router-link :to="'/update/' + item.id">Update</router-link>
+        <button v-on:click="deleteRestaurant(item.id)">Delete</button>
       </td>
     </tr>
   </table>
@@ -35,25 +36,37 @@ export default {
   components: {
     Header
   },
-  async mounted() {
-    let user = localStorage.getItem('user-info');
-    this.name = JSON.parse(user).name;
-    // console.log(this.name);
-    // console.log(user);
-    if (!user) {
-      this.$router.push({ name: 'SignUp' });
+  methods: {
+    async deleteRestaurant(id) {
+      let result = await axios.delete('http://localhost:3000/restaurants/' + id);
+      if (result.status === 200) {
+        this.loadData();
+      }
+    },
+    async loadData() {
+      let user = localStorage.getItem('user-info');
+      this.name = JSON.parse(user).name;
+      if (!user) {
+        this.$router.push({ name: 'SignUp' });
+      }
+      let result = await axios.get('http://localhost:3000/restaurants');
+      console.warn(result);
+      this.restaurants = result.data;
     }
-    let result = await axios.get('http://localhost:3000/restaurants');
-    console.warn(result);
-    this.restaurants = result.data;
+  },
+  async mounted() {
+    this.loadData();
   }
 };
+
+
 </script>
 
 <style>
 table {
   margin: auto;
 }
+
 td {
   width: 160px;
   height: 40px;
